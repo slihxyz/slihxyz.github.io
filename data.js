0d8faccf7f3f60562bb0cfc651db80cfe92645ea
slihxyz <158767946+slihxyz@users.noreply.github.com>
2025-10-14 09:28:09 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,4 @@
+// This file can be used to store data that might be reused across the application
+// For now, it's empty since we're loading all content dynamically in script.js
+// In a real application, you might store API endpoints, default settings, etc. here
+
